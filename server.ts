2c52a73e5aba752a5c038496c9160c40bedc20f3
ppa-opash <ppa.opash@gmail.com>
2024-05-06 15:21:55 +0530

    Change url

--- server.ts
+++ server.ts
@@ -126,7 +126,7 @@ export function app(): express.Express {
           const pdhtml = document.createElement('div');
           pdhtml.innerHTML = post?.postdescription || post?.metadescription;
           const talent = {
-            name: post?.title || post?.albumname || 'Healing.Tube Post',
+            name: post?.title || post?.albumname || 'Nude.girls Post',
             description: pdhtml?.textContent || 'Post content',
             image: post?.thumbfilename || post?.metaimage || post?.imageUrl || 'https://tube.nude.girls/assets/Healing-Tube-Logo.jpg',
           };
